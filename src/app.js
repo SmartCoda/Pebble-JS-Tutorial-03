d5070f1cb35ef5d02047bf4b68ef5a41092e78b4
SmartCoda <SmartCoda@users.noreply.github.com>
2015-03-04 05:46:06 +0000

    first

---
+++ src/app.js
@@ -0,0 +1,71 @@
+var UI = require('ui');
+var ajax = require('ajax');
+var Vector2 = require('vector2');
+
+var parseFeed = function(data, quantity) {
+  var items = [];
+  for(var i = 0; i < quantity; i++) {
+    // Always upper case the description string
+    var title = data.list[i].weather[0].main;
+    title = title.charAt(0).toUpperCase() + title.substring(1);
+
+    // Get date/time substring
+    var time = data.list[i].dt_txt;
+    time = time.substring(time.indexOf('-') + 1, time.indexOf(':') + 3);
+
+    // Add to menu items array
+    items.push({
+      title:title,
+      subtitle:time
+    });
+  }
+
+  // Finally return whole array
+  return items;
+};
+
+// Show splash screen while waiting for data
+var splashWindow = new UI.Window();
+
+// Text element to inform user
+var text = new UI.Text({
+  position: new Vector2(0, 0),
+  size: new Vector2(144, 168),
+  text:'Downloading weather data...',
+  font:'GOTHIC_28_BOLD',
+  color:'black',
+  textOverflow:'wrap',
+  textAlign:'center',
+	backgroundColor:'white'
+});
+
+// Add to splashWindow and show
+splashWindow.add(text);
+splashWindow.show();
+
+// Make request to openweathermap.org
+ajax(
+  {
+    url:'http://api.openweathermap.org/data/2.5/forecast?q=Swansea',
+    type:'json'
+  },
+  function(data) {
+    // Create an array of Menu items
+    var menuItems = parseFeed(data, 10);
+
+    // Construct Menu to show to user
+    var resultsMenu = new UI.Menu({
+      sections: [{
+        title: 'Current Forecast',
+        items: menuItems
+      }]
+    });
+
+    // Show the Menu, hide the splash
+    resultsMenu.show();
+    splashWindow.hide();
+  },
+  function(error) {
+    console.log('Download failed: ' + error);
+  }
+);
